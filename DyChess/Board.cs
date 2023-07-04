45976566d9abb9961469782df0cbe4065fa5114e
Dyon Beumer <Dyonunic@gmail.com>
2023-07-04 15:04:19 +0200

    stuff

--- DyChess/Board.cs
+++ DyChess/Board.cs
@@ -2,47 +2,72 @@
 {
     public class Board
     {
-        public int[,] Positions { get; set; } = null!;
-        public record Unit(IMoveable Type, Player Player, Position Position);
+        public record Unit(IMoveable Type, Player Player);
         public Unit[] units { get; set; } = null!;
         public Board()
         {
-            Setup();
             InitPositions();
+            Setup();
         }
+        public (string, Unit?)[,] Positions { get; set; } = new (string, Unit?)[8,8];
 
         private void InitPositions()
         {
             //todo
+            var horizontals = new Dictionary<int, string> {
+                    { 0, "a"},
+                    { 1, "b"},
+                    { 2, "c"},
+                    { 3, "d"},
+                    { 4, "e"},
+                    { 5, "f"},
+                    { 6, "g"},
+                    { 7, "h"}
+                };
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 1; j <= 8; j++) // verticals
+                {
+                    var id = $"{horizontals[i] + "" + j}";
+                    Console.WriteLine($"Initializing {id}", id);
+                    Positions[i, j-1] = new (id, null);
+                }
+            }
+        }
+
+        private Unit? GetUnitFromPosition(string posid)
+        {
+            return null;
         }
 
         private void Setup()
         {
 
-
             var units = new List<Unit>();
             for (int i = 0; i<7; i++)
             {
-                units.Add(new Unit(new Pawn(), Player.Black, new Position(i, 1)));
-                units.Add(new Unit(new Pawn(), Player.White, new Position(i, 6)));
+                units.Add(new Unit(new Pawn(), Player.Black));  //a7 t/m h7
+                units.Add(new Unit(new Pawn(), Player.White));  //a2 t/m h2
             }
 
-            units.Add(new Unit(new Rook(), Player.Black, new Position(0, 0)));
-            units.Add(new Unit(new Rook(), Player.Black, new Position(7, 0)));
-            units.Add(new Unit(new Rook(), Player.White, new Position(0, 7)));
-            units.Add(new Unit(new Rook(), Player.White, new Position(7, 7)));
-            units.Add(new Unit(new Knight(), Player.Black, new Position(1, 0)));
-            units.Add(new Unit(new Knight(), Player.Black, new Position(6, 0)));            
-            units.Add(new Unit(new Knight(), Player.White, new Position(1, 7)));
-            units.Add(new Unit(new Knight(), Player.White, new Position(6, 7)));            
-            units.Add(new Unit(new Bishop(), Player.Black, new Position(2, 0)));
-            units.Add(new Unit(new Bishop(), Player.Black, new Position(5, 0)));            
-            units.Add(new Unit(new Bishop(), Player.White, new Position(2, 7)));
-            units.Add(new Unit(new Bishop(), Player.White, new Position(5, 7)));
-            units.Add(new Unit(new Queen(), Player.White, new Position(3, 7)));
-            units.Add(new Unit(new Queen(), Player.Black, new Position(3, 0)));
-            units.Add(new Unit(new King(), Player.White, new Position(4, 7)));
-            units.Add(new Unit(new King(), Player.Black, new Position(4, 0)));
+            Positions[0, 7].Item2 = new Unit(new Rook(), Player.Black); //a8
+
+            units.Add(new Unit(new Rook(), Player.Black));      //h8
+            units.Add(new Unit(new Rook(), Player.White));      //a1 
+            units.Add(new Unit(new Rook(), Player.White));      //h1
+            units.Add(new Unit(new Knight(), Player.Black));    //b8
+            units.Add(new Unit(new Knight(), Player.Black));    //g8       
+            units.Add(new Unit(new Knight(), Player.White));    //b1
+            units.Add(new Unit(new Knight(), Player.White));    //g1           
+            units.Add(new Unit(new Bishop(), Player.Black));    //c8
+            units.Add(new Unit(new Bishop(), Player.Black));    //f8       
+            units.Add(new Unit(new Bishop(), Player.White));    //c1
+            units.Add(new Unit(new Bishop(), Player.White));    //f1
+            units.Add(new Unit(new Queen(), Player.White));     //d1
+            units.Add(new Unit(new Queen(), Player.Black));     //d8
+            units.Add(new Unit(new King(), Player.White));      //e1
+            units.Add(new Unit(new King(), Player.Black));      //e8
 
             this.units = units.ToArray();
         }
@@ -66,24 +91,7 @@
                 Console.WriteLine("<Color>:<Piece>"); // TODO: Add DestinationEvent Data
                 Console.WriteLine("Pawn = P, Rook = R, Knight = N, Bishop = B, Queen = Q, King = K, * = empty");
 
-                var horizontals = new Dictionary<int, string> { 
-                    { 0, "a"}, 
-                    { 1, "b"},
-                    { 2, "c"},
-                    { 3, "d"},
-                    { 4, "e"},
-                    { 5, "f"},
-                    { 6, "g"},
-                    { 7, "h"}
-                };
 
-                for (int i = 0; i < 7; i++)
-                {
-                    for(int j = 1; j <= 8; j++)
-                    {
-                        Console.WriteLine($"{horizontals[i]+ ""+ j}");
-                    }
-                }
             }
         }
 
